2d7482ed2a602eb770e4128c1fbfdd70816bfb12
RichardPitts1 <Richardepitts@gmail.com>
2019-08-31 15:59:34 -0500

    -

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @articles = Article.all
+    erb :index
   end
+
+  # Shows an individual article
+  get '/show' do 
+    erb :show
+  end
+
+
+  # Shows all articles
+  get '/index' do 
+    @articles = Article.all
+    erb :index
+  end
+
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @desiredArticle = Article.find_by_id(params[:id])
+    erb :show.erb
+  end
+
+
+
+
 end
---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+article1 = Article.create({
+    title: "title1",
+    content: "content1"
+})
+
+article2 = Article.create({
+    title: "title2",
+    content: "content2"
+})
+
+article3 = Article.create({
+    title: "title3",
+    content: "content3"
+})
+
+article4 = Article.create({
+    title: "title4",
+    content: "content4"
+})
+
+article5 = Article.create({
+    title: "title5",
+    content: "content5"
+})
+
+article6 = Article.create({
+    title: "title6",
+    content: "content6"
+})
